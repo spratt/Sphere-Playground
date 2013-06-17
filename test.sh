573a8c3e3c2368449b571cdf6698d47487dea88f
Simon Pratt <spratt@scs.carleton.ca>
2013-06-17 13:36:24 -0400

    Now specifying number of test points

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-./gen_points.sh 30 $1 > points.txt
+./gen_points.sh $1 $2 > points.txt
 cat points.txt | qhull i | ./facets_to_edges.py | cat points.txt - | ./all_pairs.py
 rm points.txt
