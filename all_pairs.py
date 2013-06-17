32675685eb4fdcd82bb2335e8443e172f8362c4a
Simon Pratt <simon@pr4tt.com>
2013-06-17 00:03:57 -0400

    Implemented Floyd-Warshall

--- all_pairs.py
+++ all_pairs.py
@@ -8,7 +8,8 @@ Given the output of rbox followed by a list of edges, compute
 all-pairs shortest paths on the edges.
 """
 import numpy as np
-from sys import stdin
+from sys import stdin, maxint, exit
+from math import sqrt
 
 lines = []
 
@@ -20,19 +21,53 @@ lines.pop(0)
 n = int(lines.pop(0))
 vertices = []
 
-while n > 0:
-    n -= 1
-    vertices.append(tuple(lines.pop(0).split(' ')))
+for _ in range(n):
+    vertices.append(tuple(map(float,lines.pop(0).split(' '))))
 
 edges = []
     
 for line in lines:
-    edges.append(tuple(lines.pop(0).split(' ')))
-
+    edges.append(tuple(map(int,lines.pop(0).split(' '))))
+    
 dists = np.empty((n,n))
 
-INFINITY = -1
+INFINITY = maxint
 
+# distance defaults to infinity
 for row in range(n):
     for col in range(n):
         dists[row][col] = INFINITY
+
+# distance from node to itself is zero
+for i in range(n):
+    dists[i][i] = 0
+
+square = lambda x: x * x
+
+def get_dist(u,v):
+    return sqrt(square(vertices[u][0] - vertices[v][0]) +
+                square(vertices[u][1] - vertices[v][1]))
+    
+# input edge distances
+for edge in edges:
+    u = edge[0]
+    v = edge[1]
+    d = get_dist(u,v)
+    dists[u][v] = d
+    dists[v][u] = d
+
+# floyd-warshall all-pairs shortest path algorithm
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if dists[i][k] + dists[k][j] < dists[i][j]:
+                dists[i][j] = dists[i][k] + dists[k][j]
+
+# print the distances
+print "| u | v | d_e | d_g |  t  |"
+for i in range(n):
+    for j in range(i+1,n):
+        de = get_dist(i,j)
+        dg = dists[i][j]
+        t = dg / de
+        print "|%(u)3d|%(v)3d|%(de)5.3f|%(dg)5.3f|%(t)5.3f|" % {"u":i,"v":j,"de":de,"dg":dg,"t":t}
